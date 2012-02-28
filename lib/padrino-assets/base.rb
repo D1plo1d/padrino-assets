ef52e6a983baddd7e966636f986742b31c29615f
D1plo1d <d1plo1d@d1plo1d.com>
2012-02-27 19:03:19 -0500

    more

--- lib/padrino-assets/base.rb
+++ lib/padrino-assets/base.rb
@@ -7,8 +7,19 @@ module Padrino
       app.set :assets_debug, lambda{ settings.environment != :development }
       app.set :assets_url, "/assets"
       # An array of directories to search all assets
-      app.set :asset_src_files, ["js", "css", "images"].map { |dir| [ dir, ['..', 'lib', dir] ] }.flatten
+      app.set :asset_src_files, ["js", "css"].map { |dir| [ dir, ['..', 'lib', dir] ] }.flatten
 
+
+      # CSS and JS Cache-Busting Helpers
+      app.helpers do
+        def css
+          "<link href=\"/assets/app.css\" type=\"text/css\" media=\"screen\" rel=\"stylesheet\" />"
+        end
+
+        def js
+          "<script src=\"/assets/app.js\" type=\"text/javascript\" ></script>"
+        end
+      end
 
       # Configure Sprockets
       #Rack::Sprockets.configure do |config|
@@ -24,6 +35,16 @@ module Padrino
         :load_path => app.settings.asset_src_files,
         :hosted_at => '/assets'
 
+
+      # Images
+      ["/images/:image", "/img/:image"].each do |url|
+        app.get url do
+          path = File.join App.root, "images", params[:image]
+          pass unless File.exists? path
+          content_type "image/#{File.extname(path)}"
+          send_file path
+        end
+      end
     end
   end
 end
